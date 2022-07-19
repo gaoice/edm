91d40469b9e18da6b5baf9ad270c2abcdca5bb6a
gaoice <gaoice@outlook.com>
2022-07-19 21:22:49 +0800

    init
    init

---
+++ src/main/java/com/gaoice/edm/EdmApplication.java
@@ -0,0 +1,16 @@
+package com.gaoice.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoice
+ */
+@SpringBootApplication
+public class EdmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdmApplication.class, args);
+    }
+
+}
